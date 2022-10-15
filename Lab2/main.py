8e6a7e5378da48d869ed8e16edb92323df691fdd
Dang Laptop <dnng2002@gmail.com>
2022-10-15 12:50:33 +0700

    Source code lab 2

--- Lab2/main.py
+++ Lab2/main.py
@@ -6,3 +6,15 @@ def  sin1(a,alpha,beta):
 
 def  sin2(a,b,alpha):
     return math.sin(alpha)*b/a
+
+def tri(alpha,beta,gamma):
+    return math.pi
+
+def calangel(alpha,beta):
+    return math.pi-alpha-beta
+
+def s_p_1(a,b,c):
+    p = sum([a,b,c])/3
+    return math.sqrt(p*(p-a)*(p-b)*(p-c))
+def s_p_2(s,a,b):
+    return 

d59b31a4e532178a983e096877ff65ca1adba16c
DangPC <dnng2002@gmail.com>
2022-11-26 18:45:40 +0700

    data in lab2

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 import os
 
 def  sin1(a,alpha,beta):
@@ -7,14 +8,21 @@ def  sin1(a,alpha,beta):
 def  sin2(a,b,alpha):
     return math.sin(alpha)*b/a
 
-def tri(alpha,beta,gamma):
+def tri_1(alpha,beta,gamma):
     return math.pi
 
-def calangel(alpha,beta):
+def tri_2(alpha,beta):
     return math.pi-alpha-beta
 
 def s_p_1(a,b,c):
     p = sum([a,b,c])/3
     return math.sqrt(p*(p-a)*(p-b)*(p-c))
-def s_p_2(s,a,b):
-    return 
+
+def s_p_2_1(c,hc):
+    return c*hc/2
+
+def s_p_2_2(s,x):
+    return 2*s/x 
+
+def init_network(num_func, num_para):
+    return np.zeros((num_para,num_func))-1
